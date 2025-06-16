732ff398257e021f88dfa9bb6d80dce6a8af91fb
Groot1116 <117891911+Groot1116@users.noreply.github.com>
2025-06-15 23:34:23 -0700

    Update Checking.cpp

--- Checking.cpp
+++ Checking.cpp
@@ -15,6 +15,7 @@ class Checking: public Account {
         const double interest = 1/100;
         const double feeAdult = 20.99;
         const double feeSenior = 10.99;
+        double balance = 0;
 
 
 
@@ -24,4 +25,7 @@ class Checking: public Account {
             int fee = (age >= 65) ? feeSenior : feeAdult;
 
         }
+        void add(int amount) {
+                    balance += ammount;
+                }
 };
