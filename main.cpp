538b964b47fa1707bca8362e1fd3d3dfa6dbac14
Groot1116 <117891911+Groot1116@users.noreply.github.com>
2025-06-15 23:37:16 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -42,7 +42,28 @@ int main() {
 void option(int num) {
     switch(num) {
         case 1:
-            Account a1 = new Account();
+            
+            string name;
+            string email;
+            string address;
+            string city;
+            string province;
+            string zipcode;
+
+            cout << "Enter name: ";
+            cin >> name;
+            cout << "Enter email: ";
+            cin >> email;
+            cout << "Enter address: ";
+            cin >> address;
+            cout << "Enter city: ";
+            cin >> city;
+            cout << "Enter province: ";
+            cin >> province;
+            cout << "Enter zipcode: ";
+            cin >> zipcode;
+            
+            Account a1 = new Account(name, email, address, city, province, zipcode);
             
         case 2:
             string username;
@@ -55,27 +76,106 @@ void option(int num) {
             cout << "Password: ";
             cin >> password;
 
-            Account a2 = new Account(username, password);
-
             if (username != a2.email && password != a2.password) {
                 cout << "Account Not Found";
             }
             else {
-                cout << "Hi " << a1.name << endl << "Account has been created;
+                Chequing ch = new Chequing();
+                cout << "Hi " << a1.name << endl << "Chequing Account has been created;
+                    
             }
 
         
         case 3:
+            string username;
+            string password;
             
+            cout << "Username: ";
+            cin >> username;
+            cout << \n;
 
-        
-        
+            cout << "Password: ";
+            cin >> password;
 
+           
+            if (username != a2.email && password != a2.password) {
+                cout << "Account Not Found";
+            }
+            else {
+                Saving sv = new Saving();
+                cout << "Hi " << a1.name << endl << "Saving Account has been created;
+                    
+            }
         case 4:
+            string username;
+            string password;
+            
+            cout << "Username: ";
+            cin >> username;
+            cout << \n;
+
+            cout << "Password: ";
+            cin >> password;
+
+           
+            if (username != a2.email && password != a2.password) {
+                cout << "Account Not Found";
+            }
+            else {
+                Business bs = new Business();
+                cout << "Hi " << a1.name << endl << "Business Account has been created;          
+            }
 
         case 5:
+            string opt;
+            double amount;
+            cout << "Choose the account: ";
+            cin >> opt;
 
+            cout << "Choose amount: ";
+            cin >> amount;
+
+            if (opt == "chequing") {
+                ch.add(amount);
+            }
+
+            else if (opt == "saving") {
+                sv.add(amount);
+            }
+
+            else if (opt == "business) {
+                bs.add(amount);
+            }
+
+            else {
+                cout << "please select valid account" << endl;
+            }
+
+                
         case 6:
+            string opt;
+            double amount;
+            cout << "Choose the account: ";
+            cin >> opt;
+
+            cout << "Choose amount: ";
+            cin >> amount;
+
+            if (opt == "chequing") {
+                cout << "balance: " << ch.balance;
+            }
+
+            else if (opt == "saving") {
+                cout << "balance: " << sv.balance;
+            }
+
+            else if (opt == "business) {
+                cout << "balance: " << bs.balance;
+            }
+
+            else {
+                cout << "please select valid account" << endl;
+            }
 
         case 7:
 
