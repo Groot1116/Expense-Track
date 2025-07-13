123d0ccce4b802af9d9a01b46f39a5e6248a9fe6
Groot1116 <117891911+Groot1116@users.noreply.github.com>
2025-07-13 15:53:57 -0700

    Update Saving.java

--- Saving.java
+++ Saving.java
@@ -4,12 +4,14 @@ public class Saving extends Account {
     private final int numberOfTransaction = 3;
     private double balance = 0;
 	private final double interest = 3/100;
+	private String username;
+	private String password;
 
 	
 	
 	
-	public Saving() {
-		getFee()
+	public Saving(String username, String password) {
+		super(username, password);
 	}
 	
 	public void add(double amount) {
