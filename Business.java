e969eb7a7b19dfc7fcb275ce225ff129d182aae1
Groot1116 <117891911+Groot1116@users.noreply.github.com>
2025-07-13 15:54:47 -0700

    Update Business.java

--- Business.java
+++ Business.java
@@ -3,11 +3,12 @@ package Frontend;
 public class Business extends Account {
 	private double balance = 0;
 	private final double interest = 1/100;
+	private String username;
+	private String password;
 	
 	
-	
-	public Business() {
-		getFee();
+	public Business(String username, String password) {
+		super(username, password);
 	}
 	
 	public void add(double amount) {
