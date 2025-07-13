d5d399a08453f460f816fe085d7d4a75b2f7cd2d
Groot1116 <117891911+Groot1116@users.noreply.github.com>
2025-07-13 15:52:57 -0700

    Update Chequing.java

--- Chequing.java
+++ Chequing.java
@@ -5,11 +5,15 @@ public class Chequing extends Account{
 	private final double interest = 1/100;
 	private double balance = 0;
 	private double fee;
+	private String username;
+	private String password;
 	
 	
 	
-	public Chequing() {
-		fee = getFee();	
+	public Chequing(String username, String password) {
+        super(username, password);
+
+    }
 	}
 	
 	public void add(double amount) {
